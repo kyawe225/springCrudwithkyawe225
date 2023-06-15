a8475b6c3400a31393f88376fb7fcb89c3c0055e
kyawe225 <kyawe225@gmail.com>
2023-06-15 09:46:41 +0630

    name changes

--- sampleStudentCrud/src/main/java/com/example/demo/SampleApplication.java
+++ sampleStudentCrud/src/main/java/com/example/demo/SampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MdfkApplication {
+public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MdfkApplication.class, args);
+		SpringApplication.run(SampleApplication.class, args);
 	}
 
 }
